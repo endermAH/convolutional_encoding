b3831b34857c2cf0729352b82e8e34c642c868ac
Евгений Курятов <evg.kur@MacBook-Pro-Evgenij.local>
2019-12-24 13:39:18 +0300

    mem_buffer_length

--- encoder.c
+++ encoder.c
@@ -1,1 +1,20 @@
-#include <iostream.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+const char *INPUT = "1001000"
+
+const char *G1 = "1"
+const char *G2 = "11"
+
+void main() {
+  char mem_buffer = NULL
+  int mem_buffer_length = 0;
+
+  mem_buffer_length = max(strlen(G1),strlen(G2));
+
+  for( int i = 0; i <= strlen(INPUT); i++ ) {
+
+  }
+}
